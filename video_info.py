214a320a273d4d27cde57ec5f766c7132c364f33
Bhavya Jain <148266515+bhavya7612@users.noreply.github.com>
2025-01-02 19:29:27 +0530

    Add files via upload

--- video_info.py
+++ video_info.py
@@ -1,8 +1,22 @@
 import os
+from googleapiclient.discovery import build
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api._errors import TranscriptsDisabled, NoTranscriptAvailable, NoTranscriptFound
 from dotenv import load_dotenv
 load_dotenv()
+
+yt_api_key=os.getenv("YT_API_KEY")
+youtube = build('youtube', 'v3', developerKey=yt_api_key)
+
+# get video info through api_key
+def get_video_title(video_id):
+    try:
+        request=youtube.videos().list(part="snippet", id=video_id)
+        response=request.execute()
+        video_title=response['items'][0]['snippet']['title']
+        return video_title
+    except Exception as e:
+        print(f"error is {e}")
 
 # fetch transcript through youtube-transcript-api
 def get_video_transcript(video_id):                                                # method-1
